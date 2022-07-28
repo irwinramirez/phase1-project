4ab419cb78777d555984102a4179e33496f08639
Irwin Ramirez <Irwinramirez23@gmail.com>
2022-07-28 14:49:23 -0400

    added a meaninful message

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 <html lang="en">
   <head>
     <meta name="viewport" content="width=device-width, initial-scale =1.0"></meta>
+    <script defer src="index.js"></script>
+    <link rel="stylesheet" href="index.css">
+  </head>
+
+  <body>
+    <h1>Donate To Save A Plant!</h1>
+     <div id="plant-collection">
+     </div>
+     <footer></footer>
