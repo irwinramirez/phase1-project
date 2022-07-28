f7add2b6a1adbdc6665c143a4bd7ce1dc95df091
Irwin Ramirez <Irwinramirez23@gmail.com>
2022-07-28 14:53:22 -0400

    added getplants func

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function getPlants(){
+  fetch('http://localhost:3000/plants')
+  .then(response => response.json())
+  .then(resp => resp.forEach(plant => {displayPlant(plant)
+    mouseEvent()
+    }
+  ))
+}
